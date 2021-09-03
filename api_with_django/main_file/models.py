10ca2d8e8d563de1d2126f423ed852f78eb34df8
shubh <verma.iamshubh@gmail.com>
2021-09-03 12:41:53 +0530

    initial commit

--- api_with_django/main_file/models.py
+++ api_with_django/main_file/models.py
@@ -6,3 +6,4 @@ from django.db import models
 class Todo(models.Model):
     task = models.CharField(max_length=30)
     description = models.CharField(max_length=150)
+    Client = models.CharField(max_length=150)
